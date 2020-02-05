127a43285e134b28f0125b51cb0049c5044ca8e1
Mobile Apps <moshiur.mgmh@gmail.com>
2020-02-05 20:58:51 +0600

    Initial Commit 2

--- app/src/main/java/com/moshiurcse/locationapimvvm/MainActivity.java
+++ app/src/main/java/com/moshiurcse/locationapimvvm/MainActivity.java
@@ -11,13 +11,28 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
     //Find accurate location
     private FusedLocationProviderClient providerClient;
     private LocationViewModel locationViewModel;
+    private LocationCallback locationCallback=new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +68,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(Location location) {
                 if(location==null){
+
+                    createLocationRequest();
                     return;
                 }
+
+
                 locationViewModel.setLocationMutableLiveData(location);
                 /*double lat=location.getLatitude();
                 double lng=location.getLongitude();
@@ -66,6 +85,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void createLocationRequest() {
+        LocationRequest locationRequest=LocationRequest.create();
+        locationRequest.setInterval(10000);//mili second
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setNumUpdates(1); //Not require for quick requst
+
+        LocationSettingsRequest locationSettingsRequest=new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build();
+        SettingsClient settingsClient=LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> responseTask=settingsClient.checkLocationSettings(locationSettingsRequest);
+        responseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+
+                getDeviceLastLocationUpdates();
+            }
+        }) ;
+        responseTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+
+
+            }
+        });
+
+    }
+
+    private void getDeviceLastLocationUpdates() {
     }
 
 
